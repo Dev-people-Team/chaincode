1667fe928384e416052ef632d10df93dd35cfff9
jeju2025 <ego.abraxas@gmail.com>
2022-04-08 10:18:26 +0000

    fist

---
+++ application/addToWallet.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const { Wallets } = require("fabric-network");
+const path = require("path");
+
+async function main() {
+    // Main try/catch block
+    try {
+        // A wallet stores a collection of identities
+
+        const username = "kihyun1";
+
+        const wallet = await Wallets.newFileSystemWallet("wallet");
+        
+        const checkidentity = await wallet.get(username);
+        if (checkidentity) {
+            console.log(
+                `An identity for the user ${username} already exists in the wallet`
+            );
+            return;
+        }
+
+        // Identity to credentials to be stored in the wallet
+        const credPath = path.join(__dirname, "msp");
+        const certificate = fs
+            .readFileSync(
+                path.join(
+                    credPath,
+                    "/org3.example.com/users/signcerts/User1@org3.example.com-cert.pem"
+                )
+            )
+            .toString();
+        const privateKey = fs
+            .readFileSync(
+                path.join(credPath, "/org3.example.com/users/keystore/priv_sk")
+            )
+            .toString();
+
+        // Load credentials into wallet
+        const identityLabel = username;
+
+        const identity = {
+            credentials: {
+                certificate,
+                privateKey,
+            },
+            mspId: "Org3MSP",
+            type: "X.509",
+        };
+
+        await wallet.put(identityLabel, identity);
+        console.log(`Successfully import an user(${username}) into the wallet`);
+    } catch (error) {
+        console.log(`Error adding to wallet. ${error}`);
+        console.log(error.stack);
+    }
+}
+
+main();
---
+++ network/deployCC.sh
@@ -0,0 +1,207 @@
+#!/bin/bash
+
+C_YELLOW='\033[1;33m'
+C_BLUE='\033[0;34m'
+C_RESET='\033[0m'
+
+# subinfoln echos in blue color
+function infoln() {
+  echo -e "${C_YELLOW}${1}${C_RESET}"
+}
+
+function subinfoln() {
+  echo -e "${C_BLUE}${1}${C_RESET}"
+}
+
+# add PATH to ensure we are picking up the correct binaries
+export PATH=${HOME}/fabric-samples/bin:$PATH
+export FABRIC_CFG_PATH=${PWD}/config
+
+# Chaincode config variable
+
+CC_NAME="teamate"
+CC_SRC_PATH="./../contract"
+CC_RUNTIME_LANGUAGE="golang"
+CC_VERSION="5"
+CHANNEL_NAME="mychannel"
+
+
+## package the chaincode
+infoln "Packaging chaincode"
+set -x
+peer lifecycle chaincode package ${CC_NAME}.tar.gz --path ${CC_SRC_PATH} --lang ${CC_RUNTIME_LANGUAGE} --label ${CC_NAME}_${CC_VERSION} >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+
+## Install chaincode on peer0.org1
+infoln "Installing chaincode on peer0.org1..."
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+set -x
+peer lifecycle chaincode install ${CC_NAME}.tar.gz >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+
+
+## Install chaincode on peer0.org2
+infoln "Installing chaincode on peer0.org2..."
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=localhost:9051
+
+set -x
+peer lifecycle chaincode install ${CC_NAME}.tar.gz >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+
+
+## Install chaincode on peer0.org3
+infoln "Installing chaincode on peer0.org3..."
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org3MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
+export CORE_PEER_ADDRESS=localhost:11051
+
+set -x
+peer lifecycle chaincode install ${CC_NAME}.tar.gz >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+
+
+set -x
+peer lifecycle chaincode queryinstalled >&log.txt  
+{ set +x; } 2>/dev/null
+PACKAGE_ID=$(sed -n "/${CC_NAME}_${CC_VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
+
+
+
+## approve the definition for org1
+infoln "approve the definition on peer0.org1..."
+
+ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+set -x
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${CC_VERSION} --package-id ${PACKAGE_ID} --sequence 1 >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+
+
+## approve the definition for org2
+infoln "approve the definition on peer0.org2..."
+
+ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=localhost:9051
+
+set -x
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${CC_VERSION} --package-id ${PACKAGE_ID} --sequence 1 >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+
+
+## approve the definition for org3
+infoln "approve the definition on peer0.org3..."
+
+ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org3MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
+export CORE_PEER_ADDRESS=localhost:11051
+
+set -x
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${CC_VERSION} --package-id ${PACKAGE_ID} --sequence 1 >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+
+
+
+## check commitreadiness
+peer lifecycle chaincode checkcommitreadiness --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${CC_VERSION} --sequence 1 --tls --cafile $ORDERER_CA --output json
+
+
+## commit the chaincode definition
+infoln "commit the chaincode definition"
+
+PEER_CONN_PARMS="--peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt --peerAddresses localhost:11051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt"
+
+set -x
+peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} $PEER_CONN_PARMS --version ${CC_VERSION} --sequence 1 >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+
+peer lifecycle chaincode querycommitted --channelID $CHANNEL_NAME --name ${CC_NAME} --cafile $ORDERER_CA
+
+
+# ## TEST1 : Invoking the chaincode
+# infoln "TEST1 : Invoking the chaincode"
+# set -x
+# peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n ${CC_NAME} $PEER_CONN_PARMS -c '{"function":"addUser","Args":["abc@gmail.com"]}' >&log.txt
+# { set +x; } 2>/dev/null
+# cat log.txt
+# sleep 3
+
+# ## TEST2 : Invoking the chaincode
+# infoln "TEST2 : Invoking the chaincode"
+# set -x
+# peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n ${CC_NAME} $PEER_CONN_PARMS -c '{"function":"addRating","Args":["abc@gmail.com","myproj","300"]}' >&log.txt
+# { set +x; } 2>/dev/null
+# cat log.txt
+# sleep 3
+
+# ## TEST3 : Invoking the chaincode
+# infoln "TEST3 : Invoking the chaincode"
+# set -x
+# peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n ${CC_NAME} $PEER_CONN_PARMS -c '{"function":"addRating","Args":["abc@gmail.com","myproj2","500"]}' >&log.txt
+# { set +x; } 2>/dev/null
+# cat log.txt
+# sleep 3
+
+infoln "TEST1 : Mint token"
+set -x
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n ${CC_NAME} $PEER_CONN_PARMS -c '{"function":"mint","Args":[]}' >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+sleep 3
+
+infoln "TEST2 : Check Client"
+set -x
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n ${CC_NAME} $PEER_CONN_PARMS -c '{"function":"clientAccountID","Args":[]}' >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+sleep 3
+
+infoln "TEST3 : Check token balance of account"
+set -x
+peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"function":"balanceOf","Args":["eDUwOTo6Q049QWRtaW5Ab3JnMy5leGFtcGxlLmNvbSxPVT1hZG1pbixMPVNhbiBGcmFuY2lzY28sU1Q9Q2FsaWZvcm5pYSxDPVVTOjpDTj1jYS5vcmczLmV4YW1wbGUuY29tLE89b3JnMy5leGFtcGxlLmNvbSxMPVNhbiBGcmFuY2lzY28sU1Q9Q2FsaWZvcm5pYSxDPVVT"]}' >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+sleep 3
+
+infoln "TEST4 : Check token balance of account(using function)"
+set -x
+peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"function":"clientAccountBalance","Args":[]}' >&log.txt
+{ set +x; } 2>/dev/null
+cat log.txt
+sleep 3
